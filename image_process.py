7a9055afaa959ff391da57fae4e2ad5ad2ed8680
Ryo Kamiya <kamiya03130313@icloud.com>
2024-04-25 13:27:53 +0900

    CSS, JSファイルを作成

--- image_process.py
+++ image_process.py
@@ -1,10 +1,10 @@
 import cv2
 from datetime import datetime
 
-
-def canny(image):
-    return cv2.Canny(image, 100, 200)
-
+# 仮の画像処理関数
 def ML(image):
     answer = datetime.now().strftime("%Y_%m_%d%_H_%M_%S_") +'ChatGPTの回答'
     return answer
+
+def canny(image):
+    return cv2.Canny(image, 100, 200)
